66af485edc17cf1c8ec7f5f6e61d5743768e311d
Adi SK <skadimoolam@gmail.com>
2020-06-18 10:28:47 +0530

    initial commit

---
+++ src/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Skadimoolam\CrudGenerator;
+
+class Test
+{
+    public static function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
